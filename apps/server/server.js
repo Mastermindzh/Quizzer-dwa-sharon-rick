fb2312dc8ae7df60fac169a8af76c20d42f2f5c4
mastermindzh <info@rickvanlieshout.com>
2017-11-01 00:50:50 +0100

    made the example :)
    1. start all apps
    2. go to localhost:8004/play
    3. (different browser) go to localhost:8001/websocketTestCall
    4. notice the question change to "this is the neq question"
    5. ???
    6. profit.

--- apps/server/server.js
+++ apps/server/server.js
@@ -6,23 +6,44 @@ var BodyParser = require('body-parser');
 var Swaggerize = require('swaggerize-express');
 var Path = require('path');
 
+var io = require('socket.io')(Http);
 var App = Express();
-
-var Server = Http.createServer(App);
+var Server = Http.Server(App);
 
 App.use(BodyParser.json());
 App.use(BodyParser.urlencoded({
-    extended: true
+  extended: true
 }));
+
+// we need CORS because we're working from other domains (e.g react runs from your pc)
+io.origins('*:*');
+App.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 App.use(Swaggerize({
-    api: Path.resolve('./config/swagger.yml'),
-    handlers: Path.resolve('./handlers')
+  api: Path.resolve('./config/swagger.yml'),
+  handlers: Path.resolve('./handlers')
 }));
 
-Server.listen(8000, function () {
-    App.swagger.api.host = this.address().address + ':' + this.address().port;
-    /* eslint-disable no-console */
-    console.log('App running on %s:%d', this.address().address, this.address().port);
-    /* eslint-disable no-console */
+var privateQuiz = io.of("/my-private-quiz");
+
+App.get('/websocketTestCall', (req, res) => {
+  privateQuiz.emit('new-question','this is the new question')
+  res.send('websocket message fired!')
 });
+
+privateQuiz.on('connection', function(client){
+  console.log('a client connected');
+  client.on("disconnect", () => console.log("a client disconnected"));
+});
+
+Server.listen(8080, function () {
+  App.swagger.api.host = this.address().address + ':' + this.address().port;
+  console.log('App running on %s:%d', this.address().address, this.address().port);
+});
+
+io.listen(Server);
