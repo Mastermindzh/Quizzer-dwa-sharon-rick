6c865bcc1fedc76ac8ed6e58faf191c8fb01ca7d
Sharon Franke <sharonfranke94@gmail.com>
2017-11-02 22:45:36 +0100

    big rework on schema's, might break stuff, have fun ^.^

--- definitions/mongoose schemas/quiz.js
+++ definitions/mongoose schemas/quiz.js
@@ -1,4 +1,46 @@
 var mongoose = require('mongoose');
+
+var answerSchema = new mongoose.Schema({
+  teamId: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'teams',
+    required: true
+  },
+  answer: {
+    type: String,
+    minlength: 1, //Empty answers are not allowed
+    required: true
+  },
+  approved: Boolean
+})
+
+var questionSchema = new mongoose.Schema({
+  questionId: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'questions',
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['Open', 'Closed', 'Queued'], //Open-> open for submission of answers, Closed -> question is done, Queued -> quizmaster has already determined this question will be in this round.
+    required: true
+  },
+  answers: [answerSchema]
+})
+
+var roundSchema = new mongoose.Schema({
+  _id: {
+    type: Number,
+    required: true
+  },
+  categories: [{
+    type: mongoose.Schema.ObjectId,
+    ref: 'categories',
+  }],
+  questions: [questionSchema]
+})
+
+
 var quizSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -18,52 +60,20 @@ var quizSchema = new mongoose.Schema({
     type: mongoose.Schema.ObjectId,
     ref: 'teams'
   }],
-  rounds: [
-    {
-      _id: {
-        type: Number,
-        required: true
-      },
-      categories: [{
-        type: mongoose.Schema.ObjectId,
-        ref: 'categories',
-      }],
-      questions: [{
-        questionId: {
-          type: mongoose.Schema.ObjectId,
-          ref: 'questions',
-          required: true
-        },
-        status: {
-          type: String,
-          enum: ['Open', 'Closed', 'Queued'], //Open-> open for submission of answers, Closed -> question is done, Queued -> quizmaster has already determined this question will be in this round.
-          required: true
-        },
-        answers: [{
-          teamId: {
-            type: mongoose.Schema.ObjectId,
-            ref: 'teams',
-            required: true
-          },
-          answer: {
-            type: String,
-            minlength: 1, //Empty answers are not allowed
-            required: true
-          },
-          approved: Boolean
-        }],
-      }]
-    }
-  ]
+  rounds: [roundSchema]
 });
 
-quizSchema.path('rounds').schema.path('categories').validate(function(categories){
-  if(categories.length !== 3){return false}
+quizSchema.path('rounds').schema.path('categories').validate(function (categories) {
+  if (categories.length !== 3) {
+    return false
+  }
   return true;
 }, 'There need to be three categories in a round.')
 
-quizSchema.path('rounds').schema.path('questions').validate(function(questions){
-  if(questions.length >12){return false}
+quizSchema.path('rounds').schema.path('questions').validate(function (questions) {
+  if (questions.length > 12) {
+    return false
+  }
   return true;
 }, 'There cannot be more than 12 questions in a round.')
 
